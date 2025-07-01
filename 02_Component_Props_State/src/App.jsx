f8b12e1229895e648b9a89c8f089ca1b99d1d4f6
JangwooPark <p.jangwoo@gmail.com>
2025-07-01 10:13:41 +0900

    2일차-10시

---
+++ 02_Component_Props_State/src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import Main from './components/Main'
+
+function App() {
+  
+
+  return (
+    <>
+      <Header />
+       초기 셋팅 완료 
+      <Main />
+      <Footer />
+    </>
+  )
+}
+
+export default App
