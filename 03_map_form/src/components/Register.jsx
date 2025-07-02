8aca4bc28ada98e2783c2efa5c3341b096fc59b6
JangwooPark <p.jangwoo@gmail.com>
2025-07-02 15:45:45 +0900

    3Day-오후 4시

--- 03_map_form/src/components/Register.jsx
+++ 03_map_form/src/components/Register.jsx
@@ -11,29 +11,31 @@ import { useState } from "react"
 
 const Register = () => {
 // 상태(state, 값) 선언, 상태가 변경되면 자동으로 JSX가 랜더링됨  
-const [name, setName] = useState("이름"); 
+const [name, setName] = useState(""); 
 const [birth, setBirth] = useState(""); 
 const [country, setCountry] = useState(""); 
 const [bio, setBio] = useState(""); 
 
 // JavaScript 에서 함수를 3가지 방법으로 생성 할 수 있다.  ES6 (2015) <== 많은 변화. 
 // 1. 순수 함수 선언  : 1. 함수 선언식 (Function Declaration)
-function onChangeName () {
-    
+function onChangeName (e) {
+    // console.log("이름 호출됨"); 
+    // console.log(e); 
+    setName(e.target.value);
 }; 
 
 // 2. 상수 에 함수를 처리 :  2. 함수 표현식 (Function Expression)
-const onChangeBirth = function () {
-
+const onChangeBirth = function (e) {
+    setBirth(e.target.value);
 }
 
-// 3. 화살표 함수    <=== 근래에 제일 많이 사용하는 함수 표현 방식  
-const onChangeCountry = () => {
-
+// 3. 화살표 함수 (Arrow Function) — ES6   <=== 근래에 제일 많이 사용하는 함수 표현 방식  
+const onChangeCountry = (e) => {
+    setCountry(e.target.value);
 }
 
-const onChangeBio = () => {
-    
+const onChangeBio = (e) => {
+    setBio(e.target.value); 
 }
 
 
@@ -43,30 +45,35 @@ const onChangeBio = () => {
         <hr /> 
         <div>
             <input
+                value={name}
                 onChange={onChangeName}
                 placeholder="이름을 입력 하세요"
             />
         </div>
         <p />
         <div>
-            <input type="date" 
+            <input type="date"
+                value={birth} 
                 onChange={onChangeBirth}
             /> 
 
         </div>
         <p />
         <div>
-            <select onChange={onChangeCountry}>
-                <option></option>
-                <option>한국</option>
-                <option>미국</option>
-                <option>영국</option>
+            <select 
+                value={country}
+                onChange={onChangeCountry}>
+                <option value=""></option>
+                <option value="kr">한국</option>
+                <option value="us">미국</option>
+                <option value="en">영국</option>
                 
             </select>
         </div>
         <p />
         <div>
-            <textarea 
+            <textarea
+                value={bio} 
                 onChange={onChangeBio}
             /> 
 
