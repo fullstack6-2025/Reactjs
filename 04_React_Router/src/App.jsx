672a5ec215318b740f82d9822ea1d39c88e3ca0c
JangwooPark <p.jangwoo@gmail.com>
2025-07-03 11:02:38 +0900

    4Day-11시

--- 04_React_Router/src/App.jsx
+++ 04_React_Router/src/App.jsx
@@ -1,9 +1,38 @@
 
+// React Router  : Link 요청에 대해서 해당 페이지를 처리해주는 기능 
+/*
+  SPA : Single Page Application : Link <== 컴포넌트, 페이지(컴포넌트의 묶음) 를 호출 
+  MPA : Multi Page Application  : a    <== 전체 페이지를 호출  
+
+  Router 설치 : npm install react-router-dom           <=== 5.x , 6,x , 7.x (현재)
+    설치 이후 확인 : package.json => dependencies 에서 확인 
+
+  Router 셋팅 : 
+    BrowserRouter : 리엑트의 모든 컴포넌트를 처리하는 곳에서 감싸줘야 함 , 
+                    ex) main.jsx, index.jsx 
+    Routes : 
+    Router : 
+    Link   : 
+    
+
+
+
+*/ 
+
+
+
+
 function App() {
 
   return (
     <>
-        초기 셋팅 완료
+       <h1>React Router</h1> 
+       <hr />
+
+
+
+
+
     </>
   )
 }
